cf1dac5d32aa8727535a8288cfcb030c7ec6c963
dream2879 <dream2879@naver.com>
2016-07-11 09:14:03 +0900

    20160711

--- src/main/java/com/certiware/backend/model/main/ManModel.java
+++ src/main/java/com/certiware/backend/model/main/ManModel.java
@@ -4,5 +4,17 @@ public class ManModel {
 	
 	private String month;
 	private int amount;
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
 
 }
--- src/main/java/com/certiware/backend/model/main/StaticModel.java
+++ src/main/java/com/certiware/backend/model/main/StaticModel.java
@@ -1,5 +1,26 @@
 package com.certiware.backend.model.main;
 
+import java.util.List;
+
 public class StaticModel {
+	
+	private List<AmountModel> amountModel;
+	private List<ManModel> manModel;
+	public List<AmountModel> getAmountModel() {
+		return amountModel;
+	}
+	public void setAmountModel(List<AmountModel> amountModel) {
+		this.amountModel = amountModel;
+	}
+	public List<ManModel> getManModel() {
+		return manModel;
+	}
+	public void setManModel(List<ManModel> manModel) {
+		this.manModel = manModel;
+	}
+	
+	
+	
+	
 
 }
